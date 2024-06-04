5f7e4527d874a4fb920b3618abcdbb71abc1b68d
Aubrey Sherman <sherman.aubrey@gmail.com>
2024-06-04 14:31:47 -0400

    Added component to handle cards picked

---
+++ src/CardGame.jsx
@@ -0,0 +1,27 @@
+
+const DECK_OF_CARDS_API = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+/** CardGame
+ *
+ *  Props: deckId
+ *
+ *  State: cards picked
+ */
+function CardGame({ deckId }) {
+
+  // useEffect to fetch from API and use response to update state
+  // call the async fetch inside (without awaiting)
+
+  // event handler for button to pick a card
+
+  // TODO: Add button with event listener
+  return (
+    <div>
+    </div>
+  );
+
+
+}
+
+
+export default CardGame;
